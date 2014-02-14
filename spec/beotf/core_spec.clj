058e0894a9a77a2ede33162e453c97415e9d487f
Florian Hassanen <florian.hassanen@gmail.com>
2014-02-14 02:30:48 +0100

    simple string based implementation with quickdirty fixed tests

--- spec/beotf/core_spec.clj
+++ spec/beotf/core_spec.clj
@@ -1,18 +1,25 @@
 (ns beotf.core-spec
   (:require [speclj.core :refer :all]
-            [beotf.core :refer :all]))
+            [beotf.core :refer :all]
+            clojure.string))
+
+(defn i
+  "html identity hack: TODO proper solution"
+  [l r]
+  (let [trim (fn [s] (clojure.string/join (map clojure.string/trim (clojure.string/split-lines s))))]
+    (= (trim l) (trim r))))
 
 (describe "transformation from plain text to html"
-          (it "transforms lines to paragraphs" (pending)
-              (should= "<p>as df</p>" (beotf "as df"))
-              (should= "<p>as df</p>
-                       <p>bs df</p>"  (beotf "as df
-                                             bs df"))
-              (should= "<p>as df</p>
-                       <p>bs df</p>"  (beotf "as df
+          (it "transforms lines to paragraphs"
+              (should (i "<p>as df</p>" (beotf "as df")))
+              (should (i "<p>as df</p>
+                         <p>bs df</p>" (beotf "as df
+                                              bs df")))
+              (should (i "<p>as df</p>
+                         <p>bs df</p>" (beotf "as df
 
-                                             bs df")))
-          (it "transforms double parenthesis to actual parenthesis" (pending)
-              (should= "<p>as(bs)df</p>" (beotf "as((bs))df"))))
+                                              bs df"))))
+          (it "transforms double parenthesis to actual parenthesis"
+              (should (i "<p>as(bs)df</p>" (beotf "as((bs))df")))))
 
 (run-specs)
--- src/beotf/core.clj
+++ src/beotf/core.clj
@@ -1,9 +1,18 @@
 (ns beotf.core
-  (:gen-class))
+  (:gen-class)
+  (:require clojure.string))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
-(defn beotf [])
+(defn beotf
+  "Simple string manipulation based implementation of bare specs, transforms plain blog syntax to html"
+  [plain]
+  (let [lines           (clojure.string/split-lines plain)
+        trimmed-lines   (map clojure.string/trim lines)
+        non-empty-lines (filter #(seq %) trimmed-lines)
+        parenthesis     (map #(clojure.string/replace (clojure.string/replace % #"\(\(" "(") #"\)\)" ")") non-empty-lines)
+        paragraphs      (map #(str "<p>" % "</p>") parenthesis)]
+    (clojure.string/join paragraphs)))
